c55c733207bbd15c600fb108016cdd6936b8d1db
matttttttttthiias <mat.ruhsert.21@lehre.mosbach.dhbw.de>
2022-05-27 17:06:38 +0200

    test

--- src/components/cookie-banner/cookie-banner.tsx
+++ src/components/cookie-banner/cookie-banner.tsx
@@ -10,7 +10,15 @@ export class CookieBanner {
   render() {
     return (
       <Host>
-        <slot></slot>
+        <slot> 
+        
+        <div class="d-flex justify-content-center mt-5">
+        <div class="card p-3 cookie"><span>Wir benutzen Cookies.</span><a href="#">Mehr Informationen<i class="fa fa-angle-right ml-2"></i></a>
+        <div class="mt-4 text-right"><span class="mr-3 decline">Ablehnen</span><button class="btn btn-light btn-sm" type="button">Akzeptieren</button></div>
+        </div>
+        </div>
+
+        </slot>
       </Host>
     );
   }
