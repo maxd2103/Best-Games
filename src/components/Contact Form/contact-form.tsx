88db0d4229513eb7b3e966572d2a8e80537e5b6e
lau-byte <lau.teller.21@lehre.mosbach.dhbw.de>
2022-05-27 17:23:56 +0200

    form

--- src/components/Contact Form/contact-form.tsx
+++ src/components/Contact Form/contact-form.tsx
@@ -13,21 +13,22 @@ import { Component, h, } from '@stencil/core';
         
         <div>
         <div class="container">
-          <form action="/action_page.php">
+          <form action="">
             <label htmlFor="fname">First Name</label>
             <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
         
             <label htmlFor="lname">Last Name</label>
             <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
         
-            <label htmlFor="country">Country</label>
+            <label htmlFor="country">Region</label>
             <select id="country" name="country">
+              <option value="asia">Asia</option>
               <option value="australia">Australia</option>
-              <option value="canada">Canada</option>
+              <option value="europe">Europe</option>
               <option value="usa">USA</option>
             </select>
         
-            <label htmlFor="subject">Subject</label>
+            <label htmlFor="subject">Your Message</label>
             <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
         
             <input type="submit" value="Submit"/> 
