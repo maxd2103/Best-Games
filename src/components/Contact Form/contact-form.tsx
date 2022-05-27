39e17708c53229130782de59b9256f9b8a9333b1
gloria.tamakloe <gloria.tamakloe@bechtle.com>
2022-05-27 16:59:47 +0200

    contact form changed

--- src/components/Contact Form/contact-form.tsx
+++ src/components/Contact Form/contact-form.tsx
@@ -6,35 +6,40 @@ import { Component, h, } from '@stencil/core';
   })
   export class ContactForm {
 
-render() {
-    return
-<div>
+    render() {
+      return (
+       
 
-<h3>Contact Form</h3>
+        
+        <div>
+        <div class="container">
+          <form action="/action_page.php">
+            <label htmlFor="fname">First Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+        
+            <label htmlFor="lname">Last Name</label>
+            <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+        
+            <label htmlFor="country">Country</label>
+            <select id="country" name="country">
+              <option value="australia">Australia</option>
+              <option value="canada">Canada</option>
+              <option value="usa">USA</option>
+            </select>
+        
+            <label htmlFor="subject">Subject</label>
+            <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+        
+            <input type="submit" value="Submit"/> 
+            </form>
+            </div>
+            </div>
+             
+          
+         
 
-<div class="container">
-  <form action="/action_page.php">
-    <label for="fname">First Name</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your name..">
-
-    <label for="lname">Last Name</label>
-    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-
-    <label for="country">Country</label>
-    <select id="country" name="country">
-      <option value="australia">Australia</option>
-      <option value="canada">Canada</option>
-      <option value="usa">USA</option>
-    </select>
-
-    <label for="subject">Subject</label>
-    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-
-    <input type="submit" value="Submit">
-  </form>
-</div>
-
-</div>
+        
+     
       );
     }
   }
