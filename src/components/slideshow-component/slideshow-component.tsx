ebc8212aaba7ace39a93931c9bb3d40ccede36ce
gloria.tamakloe <gloria.tamakloe@bechtle.com>
2022-05-27 15:49:37 +0200

    slideshow bearbeitet

--- src/components/slideshow-component/slideshow-component.tsx
+++ src/components/slideshow-component/slideshow-component.tsx
@@ -7,9 +7,15 @@ import { Component, h } from '@stencil/core';
 })
 export class SlideshowComponent {
 
-  private handleClick = () => {
+  private handleClick1 = () => {
     alert('Received the button click!');
     }
+    private handleClick2 = () => {
+      alert('Received the button click!');
+      }
+      private handleClick3 = () => {
+        alert('Received the button click!');
+        }
 
   render() {
     return (
@@ -18,30 +24,30 @@ export class SlideshowComponent {
         
           <div class="mySlides fade">
             <div class="numbertext">1 / 3</div>
-            <img src="img1.jpg" style="width:100%"/>
+            <img src="img1.jpg" />
             <div class="text">Caption Text</div>
           </div>
         
           <div class="mySlides fade">
             <div class="numbertext">2 / 3</div>
-            <img src="img2.jpg" style="width:100%"/>
+            <img src="img2.jpg"/>
             <div class="text">Caption Two</div>
           </div>
         
           <div class="mySlides fade">
             <div class="numbertext">3 / 3</div>
-            <img src="img3.jpg" style="width:100%"/>
+            <img src="img3.jpg" />
             <div class="text">Caption Three</div>
           </div>
         
-          <a class="prev" onClick={this.handleClick}>&#10094;</a>
-          <a class="next" onClick={this.handleClick}>&#10095;</a>
+          <a class="prev" onClick={this.handleClick1}>&#10094;</a>
+          <a class="next" onClick={this.handleClick2}>&#10095;</a>
         </div>
         
-        <div style="text-align:center">
-          <span class="dot" onClick={this.handleClick}></span>
-          <span class="dot" onClick={this.handleClick}></span>
-          <span class="dot" onClick={this.handleClick}></span>
+        <div id="eins">
+          <span class="dot" onClick={this.handleClick1}></span>
+          <span class="dot" onClick={this.handleClick2}></span>
+          <span class="dot" onClick={this.handleClick3}></span>
         </div>
       </div>
     );
