a87ada0ea86c718c6d40df03e9717a7110aed8bc
gloria.tamakloe <gloria.tamakloe@bechtle.com>
2022-05-27 18:12:06 +0200

    nochmal gepusht

--- src/components/slideshow-component/slideshow-component.tsx
+++ src/components/slideshow-component/slideshow-component.tsx
@@ -12,41 +12,11 @@ export class SlideshowComponent {
   
   render() {
     return (
-      <div>
-       <h2>Automatic Slideshow</h2>
-<p>Change image every 2 seconds:</p>
-
-<div class="slideshow-container">
-
-<div class="mySlides fade">
-  <div class="numbertext">1 / 3</div>
-  <img src="src/components/slideshow-component/img-slideshow/memorie.png" />
-  <div class="text">Caption Text</div>
+      <div class="simpleSlides">
+  <img class="slides animate-left" src="/src/components/slideshow-component/img-slideshow/memorie.png"/>
+  <img class="slides animate-left" src="/src/components/slideshow-component/img-slideshow/spacepilot.png"/>
+  <img class="slides animate-left" src="/src/components/slideshow-component/img-slideshow/tictactoe.png"/>
 </div>
-
-<div class="mySlides fade">
-  <div class="numbertext">2 / 3</div>
-  <img src="src/components/slideshow-component/img-slideshow/img-slideshow/spacepilot.png" />
-  <div class="text">Caption Two</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">3 / 3</div>
-  <img src="src/components/slideshow-component/img-slideshow/tictactoe.png" />
-  <div class="text">Caption Three</div>
-</div>
-
-</div>
-
-
-<div>
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-</div>
-        
-  
-      </div>
     );
 
   }
